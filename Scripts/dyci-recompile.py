59a09f10939fcd8496b326a097ac01ea2aeb79a9
Paul Taykalo <tt.kilew@gmail.com>
2015-03-03 17:19:26 +0200

    Added Logging into /tmp/dyci.log on injection. Added Steps support, like described in https://github.com/DyCI/dyci-main/wiki/Dyci-Injection-Steps

--- Scripts/dyci-recompile.py
+++ Scripts/dyci-recompile.py
@@ -26,10 +26,23 @@ except:
     stderr.write("Incorrect usage. Path to .m,.h file or resource should be used as the parameter")
     exit(1)
 
+# loading it's params from the file
+XCACTIVITYPARSER_LOCATION = DYCI_ROOT_DIR + "/scripts/xcactivity-parser.py" 
+DERIVED_DATA_DIR = os.path.expanduser("~/Library/Developer/Xcode/DerivedData")
+#ARCH = "x86_64"
+
+#-----------------------------------------------------------------------------
+def dyciLOG(value, init=False):
+    with open("/tmp/dyci.log", "w+" if init else "a+") as f:
+        f.write(value + os.linesep)
+
+def dyciLOGStep(value, message = ""):
+    dyciLOG("\n>> Step #" + value + " " + message + " http://bit.ly/1KjK81p ")
+
 #----------------------------------------------------------------------------------
 # Running process
 def runAndFailOnError(stringToRun, shell=True):
-    stderr.write("Running %s" % stringToRun)
+    dyciLOG("Running\n %s" % stringToRun)
     process = Popen(stringToRun,
                 shell=shell,
                 stdout=subprocess.PIPE,
@@ -43,18 +56,51 @@ def runAndFailOnError(stringToRun, shell=True):
     if process.returncode != 0:
         sys.exit(1)
 
+    return output.rstrip(os.linesep)    
+
 #----------------------------------------------------------------------------------
-def locateCompileStringForFile(filename):
-    process = Popen([xcactivityParserLocation,"-f",filename,"-x","/Users/ptaykalo/Library/Developer/Xcode/DerivedData/Ladybug-beaxpvgiwpmzbwamzrmskgifxtmw/Logs/Build", "-a","i386"],
+def locateCompileStringForFile(original_filename):
+    currDir = os.getcwd()
+    dyciLOGStep("3.1", "Locate latest build dir")
+    os.chdir(DERIVED_DATA_DIR)    
+    dyciLOG("DerivedData is %s" % DERIVED_DATA_DIR)
+
+    lastBuildDir = DERIVED_DATA_DIR + "/" + runAndFailOnError("ls -td */Logs/Build | head -n 1")
+    dyciLOG("Last build is %s" % lastBuildDir)
+
+    dyciLOGStep("3.2", "Locate compilation parameters")
+    filename = original_filename.replace(" ", "\\ ")
+    dyciLOG("Filename prepared for injection %s" % filename)
+
+    dyciLOGStep("3.4", "Resolving ARCH of file to search")
+    ARCH_FILE = DYCI_ROOT_DIR + "/arch"
+    ARCH = "x86_64"
+    if os.path.exists(ARCH_FILE):
+        with open(ARCH_FILE) as f: ARCH = f.read()
+    else:
+        dyciLOG("Falling back to deafult arch %s" % ARCH)
+            
+    dyciLOG("Usin ARCH for search %s" % ARCH)
+    
+    command = [XCACTIVITYPARSER_LOCATION,"-f",filename,"-x",lastBuildDir, "-a", ARCH]
+    dyciLOG("Running command\n%s" % " ".join(command))
+
+    process = Popen(command,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
     compileString, err = process.communicate()
+    dyciLOG("CompileString '%s'" % compileString)
+
     compileString = compileString.rstrip(os.linesep)
     stderr.write(err)
     
     if process.returncode != 0 or compileString is None or len(compileString) == 0:
+        dyciLOG("Compile string is empty. This file wasn't compiled for speficified architecture %s, or build directory wasn't resolved correctly %s" % (ARCH, lastBuildDir) )
         stderr.write("Cannot find how this file was compiled. \nPlease, try to compile it first")
         sys.exit(1)
+
+    os.chdir(currDir)  
+    return compileString  
 
 #----------------------------------------------------------------------------------
 def removeDynamicLibsFromDirectory(dir):
@@ -129,63 +175,79 @@ def copyResource(source, dyci):
     return 0    
 
 #----------------------------------------------------------------------------------
-def filenameIsResounce(filename):
+def filenameIsResource(filename):
     return filename[-4:] == ".png" or filename[-4:] == ".jpg" or filename[-5:] == ".jpeg" or filename[-8:] == ".strings"
 
 def dyciHandleResounce(filename):
+    dyciLOG("Handling resource %s" % filename)
     resultCode = copyResource(filename, DYCI_ROOT_DIR)
     exit(resultCode)
 
 #----------------------------------------------------------------------------------
 
+def filenameIsXIB(filename):
+    return filename[-4:] == ".xib"
 
-
-# In case of resources..
-if filenameIsResounce(FILENAME): 
-   dyciHandleResounce() 
-
-#In case of xibs
-if filename[-4:] == ".xib": 
+def dyciHandleXIB(filename):
+    dyciLOG("Handling XIB %s" % filename)
     xibFilename = os.path.splitext(filename)[0] + ".nib"
     runAndFailOnError(["ibtool", "--compile", xibFilename, filename])
     resultCode = copyResource(xibFilename, DYCI_ROOT_DIR)
     os.system("rm -Rf %s" % xibFilename)
     exit(resultCode)
 
-#Storyboards also welcome
-if filename[-11:] == ".storyboard": 
+#----------------------------------------------------------------------------------
+def filenameIsStoryboard(filename):
+    return filename[-11:] == ".storyboard"
+
+def dyciHandleStoryboard(filename):
+    dyciLOG("Handling Storyboard %s" % filename)
     storyboardFileName = os.path.splitext(filename)[0] + ".storyboardc"
     runAndFailOnError(["ibtool", "--compile", storyboardFileName, filename])
     resultCode = copyResource(storyboardFileName, DYCI_ROOT_DIR)
     shutil.rmtree(storyboardFileName)
     exit(resultCode)
 
+#----------------------------------------------------------------------------------
+dyciLOG("--------------------------------------", True)
+dyciLOGStep("3", "Locate previous compilation parameters")
+dyciLOG("Injecting %s" % FILENAME)
+
+# In case of resources..
+if filenameIsResource(FILENAME): 
+   dyciHandleResounce(FILENAME) 
+
+#In case of xibs
+if filenameIsXIB(FILENAME): 
+   dyciHandleXIB(FILENAME) 
+
+#Storyboards also welcome
+if filenameIsStoryboard(FILENAME): 
+    dyciHandleStoryboard(FILENAME)
+
+
 
 # In case of header files
 # In some cases you need be able to recompile M file, when you are in header
-if filename[-2:] == ".h": filename = os.path.splitext(filename)[0] + ".m"
+if FILENAME[-2:] == ".h": FILENAME = os.path.splitext(FILENAME)[0] + ".m"
 
-# loading it's params from the file
-xcactivityParserLocation = DYCI_ROOT_DIR + "/scripts/xcactivity-parser.py" 
-
-# print "xcactivityParserLocation \n%s" % DYCI_ROOT_DIR
-# print xcactivityParserLocation + " -f " + filename + " -x "+  "/Users/ptaykalo/Library/Developer/Xcode/DerivedData/Ladybug-beaxpvgiwpmzbwamzrmskgifxtmw/Logs/Build" 
-
-# Switching to the specified working directory
 
 # Searching where is Xcode with it's Clang located
 
-compileString = locateCompileStringForFile(filename)
-
-# print "Compiler string was \n%s" % ''.join(compileString)
-# stdout.write(compileString)
-
+compileString = locateCompileStringForFile(FILENAME)
 
 #Compiling file again
-workingDir = "/Volumes/data/Projects/ladybug-ios/LadyBug"
-os.chdir(workingDir)
+dyciLOGStep("3.3", "Locate previos working directory")
+WORKING_DIR = "/Volumes/data/Projects/ladybug-ios/LadyBug"
+dyciLOG("Working dir %s" % WORKING_DIR)
+
+os.chdir(WORKING_DIR)
+
+dyciLOGStep("4", "Recompile file")
+dyciLOGStep("4.1", "Recompile file itself")
 runAndFailOnError(compileString)
 
+dyciLOGStep("4.2", "Link compiled object file to dylib")
 #Compilation was successful... performing linking
 params = re.compile("(?<!\\\\) ").split(compileString)
 clangParams = parseClangCompileParams(params)
@@ -208,12 +270,14 @@ libraryName = "dyci%s.dylib" % random.randint(0, 10000000)
 process = Popen(["xcode-select","-print-path"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
-xcodeLocation, err = process.communicate()
-xcodeLocation = xcodeLocation.rstrip(os.linesep)
+XCODE_LOCATION, err = process.communicate()
+XCODE_LOCATION = XCODE_LOCATION.rstrip(os.linesep)
+dyciLOG("Xcode Location %s" % XCODE_LOCATION)
 
 #Running linker, that will create dynamic library for us
+LIBRARY_OUTPUT_LOCATION = DYCI_ROOT_DIR + "/" + libraryName
 linkArgs = \
-[xcodeLocation + "/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang"] \
+[XCODE_LOCATION + "/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang"] \
 + ["-arch"] + [clangParams['arch']]\
 + ["-dynamiclib"]\
 + ["-isysroot"] + [clangParams['isysroot']]\
@@ -239,9 +303,11 @@ linkArgs = \
 + ["-current_version"]\
 + ["5"]\
 + ["-o"]\
-+ [DYCI_ROOT_DIR + "/" + libraryName]\
++ [LIBRARY_OUTPUT_LOCATION]\
 + ["-v"]
 
+dyciLOG("Creating .dylib")
 runAndFailOnError(linkArgs, False)
-# print "Linker arks \n%s" % ' '.join(linkArgs)
+
+dyciLOG(".dylib Compiled ")
 
